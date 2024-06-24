6c1f03aec575eb6fbdabbae541cc19a9480b9626
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-06-24 17:59:52 +0300

    finishing edit-component

--- student-manager-app/student-manager/ClientApp/src/components/Edit.js
+++ student-manager-app/student-manager/ClientApp/src/components/Edit.js
@@ -1,35 +1,135 @@
+import { useEffect, useState } from "react";
+
+const entry = {
+  id: "",
+  firstName: "",
+  lastaNme: "",
+  department: "",
+  className: "",
+  gender: 0,
+  dateOfBirth: new Date(),
+  isGraduated: false,
+  age: 0,
+};
+
 export default function Edit(props) {
+  const [data, setData] = useState({});
+  const [gender, setGender] = useState(0);
+  const [graduated, setGraduated] = useState(false);
+  const [sid, setSid] = useState("");
+
+  const updateStudent = () => {
+    console.log("The New Student Is: ", entry);
+
+    fetch("api/student/" + sid, {
+      method: "PUT",
+      body: JSON.stringify(entry),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((r) => {
+        console.log("Response for updating a student: ", r);
+        window.location = "/";
+      })
+      .catch((e) => console.log("Error updating a student: ", e));
+  };
+
+  const newData = (e) => {
+    const name_ = e.target.name;
+    let v_ = e.target.value;
+
+    if (name_ === "dateOfBirth") {
+      v_ = new Date(v_);
+      entry.age = new Date().getFullYear() - v_.getFullYear();
+    }
+
+    if (name_ === "gender") {
+      v_ = Number(v_);
+      setGender(v_);
+    }
+
+    if (name_ === "isGraduated") {
+      v_ = v_ === "true";
+      setGraduated(v_);
+    }
+
+    entry[name_] = v_;
+  };
+
+  useEffect(() => {
+    let id_ = window.location.search;
+    if (id_) {
+      id_ = id_.split("=")[1];
+    }
+
+    if (id_) {
+      setSid(id_);
+
+      fetch("api/student/" + id_)
+        .then((r) => r.json())
+        .then((d) => {
+          console.log("Student for update: ", d);
+          setGender(d.gender);
+          setGraduated(d.graduated);
+          setData(d);
+          Object.assign(entry, d);
+        })
+        .catch((e) => console.log("Error getting student for update: ", e));
+    }
+  }, []);
+
   return (
-    <section className="mt-10">
-      <h1>Edit Student</h1>
+    <section className="m-20">
+      <h1>Update Student</h1>
+
       <div className="mt-10">
         <label htmlFor="fn">First Name</label>
-        <input type="text" name="firstName" id="fn" onChange={newData} />
-      </div>
-
-      <div className="mt-10">
-        <label htmlFor="ln">Last Name</label>
-        <input type="text" name="lastName" id="ln" onChange={newData}></input>
-      </div>
-
-      <div className="mt-10">
-        <label htmlFor="department">Department</label>
         <input
           type="text"
-          name="department"
-          id="department"
+          name="firstName"
+          id="fn"
+          defaultValue={data.firstName}
           onChange={newData}
         />
       </div>
 
       <div className="mt-10">
-        <label htmlFor="cn">Class</label>
-        <input type="text" name="className" id="cn" onChange={newData} />
+        <label htmlFor="ln">Last Name</label>
+        <input
+          type="text"
+          name="lastName"
+          id="ln"
+          defaultValue={data.lastName}
+          onChange={newData}
+        />
+      </div>
+
+      <div className="mt-10">
+        <label htmlFor="cn">Class Name</label>
+        <input
+          type="text"
+          name="className"
+          id="cn"
+          defaultValue={data.className}
+          onChange={newData}
+        />
+      </div>
+
+      <div className="mt-10">
+        <label htmlFor="dp">Department</label>
+        <input
+          type="text"
+          name="department"
+          id="dp"
+          defaultValue={data.department}
+          onChange={newData}
+        />
       </div>
 
       <div className="mt-10">
         <label htmlFor="gender">Gender</label>
-        <select name="gender" id="gender" onChange={newData}>
+        <select name="gender" id="gender" value={gender} onChange={newData}>
           <option value={1}>Male</option>
           <option value={0}>Female</option>
         </select>
@@ -41,24 +141,30 @@ export default function Edit(props) {
           type="date"
           name="dateOfBirth"
           id="dob"
+          defaultValue={data.dateOfBirth ? data.dateOfBirth.split("T")[0] : ""}
           onChange={newData}
-        ></input>
+        />
       </div>
 
       <div className="mt-10">
         <label htmlFor="graduated">Is Graduated</label>
-        <select name="isGraduated" id="graduated" onChange={newData}>
-          <option value={1}>Yes</option>
-          <option value={0}>No</option>
+        <select
+          name="isGraduated"
+          id="graduated"
+          value={graduated}
+          onChange={newData}
+        >
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
         </select>
       </div>
 
-      <div className="mt-30 row p20 justify-between">
+      <div className="mt-30 row p20 justify-btw">
         <div className="btn cancel" onClick={() => (window.location = "/")}>
           Cancel
         </div>
-        <div className="btn add" onClick={addNewStudent}>
-          Add
+        <div className="btn add" onClick={updateStudent}>
+          Update
         </div>
       </div>
     </section>
